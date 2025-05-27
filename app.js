3793dcaf36fa89e2fe353e23dbbfb2255e9ad439
nottie-noe <noelee.ndlovu@gmail.com>
2025-05-28 01:26:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 const express = require('express');
 const http = require('http');
-const path = require('path');
 const socketIo = require('socket.io');
 const client = require('prom-client');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-// Serve static files from the current directory (where index.html is)
+// Serve static files (like index.html)
 app.use(express.static(path.join(__dirname)));
 
 // Prometheus metrics
 client.collectDefaultMetrics();
-
 const connectedUsersGauge = new client.Gauge({
   name: 'chat_connected_users',
   help: 'Number of connected users',
@@ -21,19 +20,12 @@ const connectedUsersGauge = new client.Gauge({
 
 io.on('connection', (socket) => {
   connectedUsersGauge.inc();
-  socket.on('disconnect', () => {
-    connectedUsersGauge.dec();
-  });
+  socket.on('disconnect', () => connectedUsersGauge.dec());
 });
 
 app.get('/metrics', async (req, res) => {
   res.set('Content-Type', client.register.contentType);
   res.end(await client.register.metrics());
-});
-
-// ✅ Serve index.html on root route
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 server.listen(3000, () => {
