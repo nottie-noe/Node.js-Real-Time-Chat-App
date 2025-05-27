3c556ce54fc64c01b452d062095b415e1179bd10
nottie-noe <noelee.ndlovu@gmail.com>
2025-05-27 22:24:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,16 +7,16 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-// Serve index.html and other static files
+// Serve index.html and other static files from current folder
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 io.on('connection', socket => {
-    console.log('User connected');
-    socket.on('disconnect', () => console.log('User disconnected'));
+  console.log('User connected');
+  socket.on('disconnect', () => console.log('User disconnected'));
 });
 
 server.listen(3000, () => console.log('Chat app running on port 3000'));
