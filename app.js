61a980793b01229bb13c1651cf3c13d85d1bede9
nottie-noe <noelee.ndlovu@gmail.com>
2025-05-27 23:39:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,33 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
-const path = require('path');
+const client = require('prom-client');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-// Serve index.html and other static files from current folder
-app.use(express.static(__dirname));
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics();
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+const connectedUsersGauge = new client.Gauge({
+  name: 'chat_connected_users',
+  help: 'Number of connected users',
 });
 
-io.on('connection', socket => {
-  console.log('User connected');
-  socket.on('disconnect', () => console.log('User disconnected'));
+io.on('connection', (socket) => {
+  connectedUsersGauge.inc();
+
+  socket.on('disconnect', () => {
+    connectedUsersGauge.dec();
+  });
 });
 
-server.listen(3000, () => console.log('Chat app running on port 3000'));
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', client.register.contentType);
+  res.end(await client.register.metrics());
+});
+
+server.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
