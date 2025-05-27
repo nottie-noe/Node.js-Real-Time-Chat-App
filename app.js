89da19ad520e7f19a829fe51292cb25aecceea6f
nottie-noe <noelee.ndlovu@gmail.com>
2025-05-28 00:59:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,14 +7,17 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
+// Enable collection of default metrics (CPU, memory, etc.)
 const collectDefaultMetrics = client.collectDefaultMetrics;
 collectDefaultMetrics();
 
+// Custom metric: number of connected users
 const connectedUsersGauge = new client.Gauge({
   name: 'chat_connected_users',
   help: 'Number of connected users',
 });
 
+// WebSocket logic
 io.on('connection', (socket) => {
   connectedUsersGauge.inc();
 
@@ -23,11 +26,18 @@ io.on('connection', (socket) => {
   });
 });
 
+// Route to expose Prometheus metrics
 app.get('/metrics', async (req, res) => {
   res.set('Content-Type', client.register.contentType);
   res.end(await client.register.metrics());
 });
 
+// Default homepage route
+app.get('/', (req, res) => {
+  res.send('Chat server is up and running!');
+});
+
+// Start the server
 server.listen(3000, () => {
   console.log('Server running on port 3000');
 });
