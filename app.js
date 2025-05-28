c709adc063825c758e943081ac22fef291fa3fb5
nottie-noe <noelee.ndlovu@gmail.com>
2025-05-29 00:51:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
+
+// Serve static files from the current directory
+app.use(express.static(__dirname));
 
 io.on('connection', socket => {
     console.log('User connected');
